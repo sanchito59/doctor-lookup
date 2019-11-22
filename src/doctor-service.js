79838c6ddbd81a48ad6443100041072a9d794603
Chris Sanchez <c.sanch7@gmail.com>
2019-11-22 10:26:47 -0800

    create doctor object, begin filling with info

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,12 +1,14 @@
 export class DoctorSearch {
     async doctorSearchByName(name) {
-        let doctorName;
+        let doctor = {};
         try {
             const response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.API_KEY}`);
             if (response.ok) {
                 const jsonResponse = await response.json();
-                doctorName = jsonResponse.data[0].profile;
-                console.log('Doctor Name: ', doctorName);
+                doctor.firstName = jsonResponse.data[0].profile.first_name;
+                doctor.lastName = jsonResponse.data[0].profile.last_name;
+                doctor.fullName = `${doctor.firstName} ${doctor.lastName}`;
+                console.log('Doctor Name: ', doctor);
             } else {
                 // conditionResult = futureErrorVariable;
                 throw new Error(response.status);
