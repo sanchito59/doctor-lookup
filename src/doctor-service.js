ec7c40674765b166ed8839a43d268e129b39679b
Chris Sanchez <c.sanch7@gmail.com>
2019-11-22 11:39:54 -0800

    correct indentation

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,30 +1,31 @@
 export class DoctorSearch {
-    async doctorSearchByName(name) {
-        let doctor = {};
-        try {
-            const response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-            if (response.ok) {
-                const jsonResponse = await response.json();
-                doctor.firstName = jsonResponse.data[0].profile.first_name;
-                doctor.lastName = jsonResponse.data[0].profile.last_name;
-                doctor.fullName = jsonResponse.data[0].practices[0].name;
-                doctor.city = jsonResponse.data[0].practices[0].visit_address.city;
-                doctor.state = jsonResponse.data[0].practices[0].visit_address.state;
-                doctor.street = jsonResponse.data[0].practices[0].visit_address.street;
-                doctor.street2 = jsonResponse.data[0].practices[0].visit_address.street2;
-                doctor.zipCode = jsonResponse.data[0].practices[0].visit_address.zip;
-                doctor.address = `${doctor.city}, ${doctor.state}; ${doctor.street}. ${doctor.street2}, ${doctor.zipCode} `;
-                doctor.phoneNumber = jsonResponse.data[0].practices[0].phones[0].number;
-                // doctor.website = jsonResponse.data[0].practices[4].website; //Most dont seem to have websites?
-                doctor.newPatients = jsonResponse.data[0].practices[0].accepts_new_patients;
-                console.log('Doctor Info: ', doctor);
-            } else {
-                // conditionResult = futureErrorVariable;
-                throw new Error(response.status);
-            }
-        } catch (error) {
-            console.error(`Better Doctor Fetch error: ${error}`);
-        }
-        return doctor;
+  async doctorSearchByName(name) {
+    let doctor = {};
+    try {
+      const response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or-portland&sort=full-name-asc&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log(jsonResponse.data[0]);
+        doctor.firstName = jsonResponse.data[0].profile.first_name;
+        doctor.lastName = jsonResponse.data[0].profile.last_name;
+        doctor.fullName = jsonResponse.data[0].practices[0].name;
+        doctor.city = jsonResponse.data[0].practices[0].visit_address.city;
+        doctor.state = jsonResponse.data[0].practices[0].visit_address.state;
+        doctor.street = jsonResponse.data[0].practices[0].visit_address.street;
+        doctor.street2 = jsonResponse.data[0].practices[0].visit_address.street2;
+        doctor.zipCode = jsonResponse.data[0].practices[0].visit_address.zip;
+        doctor.address = `${doctor.city}, ${doctor.state}; ${doctor.street}. ${doctor.street2}, ${doctor.zipCode} `;
+        doctor.phoneNumber = jsonResponse.data[0].practices[0].phones[0].number;
+        // doctor.website = jsonResponse.data[0].practices[4].website; //Most dont seem to have websites?
+        doctor.newPatients = jsonResponse.data[0].practices[0].accepts_new_patients;
+        console.log('Doctor Info: ', doctor);
+      } else {
+        // conditionResult = futureErrorVariable;
+        throw new Error(response.status);
+      }
+    } catch (error) {
+      console.error(`Better Doctor Fetch error: ${error}`);
     }
+    return doctor;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -5,13 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function () {
-    $('#searchInput').on('change', function () {
-        // event.preventDefault();
-        let search;
-        let searchInput = $('#searchInput').val();
-        search = new DoctorSearch();
-        search.doctorSearchByName(searchInput);
-        console.log('Doctor: ', doctor);
-        console.log(search.conditionSearch(searchInput));
-    });
+  $('#searchInput').on('change', function () {
+    let search;
+    let searchInput = $('#searchInput').val();
+    search = new DoctorSearch();
+    search.doctorSearchByName(searchInput);
+    console.log('Doctor: ', doctor);
+    console.log(search.conditionSearch(searchInput));
+  });
 });
