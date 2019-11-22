052284d173dc9ca83d5b8429e1b9bfdce18745fb
Chris Sanchez <c.sanch7@gmail.com>
2019-11-22 08:59:54 -0800

    add small functionality to conditionSearch, debugging

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import $ from 'jquery';
+import { DoctorSearch } from './../src/script-backend.js';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
-$(document).ready(function(){
-  console.log("hello");
-
+$(document).ready(function () {
+    $('#searchInput').on('change', function () {
+        let search;
+        let searchInput = $('#searchInput').val();
+        search = new DoctorSearch(searchInput);
+        console.log(search);
+        search.conditionSearch(search);
+    });
 });
---
+++ src/script-backend.js
@@ -0,0 +1,23 @@
+export class DoctorSearch {
+    constructor(search) {
+        this.search = search;
+    }
+
+    async conditionSearch(condition) {
+        console.log(condition);
+        let conditionResult;
+        try {
+            const response = await fetch(`https://api.betterdoctor.com/2016-03-01/conditions?user_key=${process.env.GIPHY_KEY}`);
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                conditionResult = jsonResponse;
+                console.log(conditionResult);
+            } else {
+                // conditionResult = futureErrorVariable;
+                throw new Error(response.status);
+            }
+        } catch (error) {
+            console.error(`Better Doctor Fetch error: ${error}`);
+        }
+    }
+}
