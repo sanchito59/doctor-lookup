bf85565d3a8f730c5bb50540c7ee49689a9ad2d2
Chris Sanchez <c.sanch7@gmail.com>
2019-11-23 09:19:23 -0800

    add full functionality, but code is not DRY

--- src/main.js
+++ src/main.js
@@ -5,6 +5,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function () {
+  function noSearchResult(arr) {
+    if (arr.length < 1) {
+      $('#doctorInformation').hide();
+      $('#noResultError').slideDown();
+      setTimeout(() => $('#noResultError').slideUp(), 3500);
+    }
+  }
   //Condition Search
   $('#conditionSearchButton').click(function () {
     let conditionSearchInput = $('#conditionSearch').val();
@@ -16,10 +23,50 @@ $(document).ready(function () {
     })();
 
     function getElements(response) { // currently working here
-      console.log(response);
+      $('#doctorInformation').show();
       $('#doctorInformation').text('');
       let doctorsArr = Object.keys(response.data);
-      console.log(doctorsArr);
+      noSearchResult(doctorsArr);
+      for (let i = 0; i < doctorsArr.length; i++) {
+        $('#doctorInformation').append(`
+        <div id='doctorResult'>
+          <h4> ${response.data[i].profile.first_name} <span class='middleName' id='missingMiddleName'>${response.data[i].profile.middle_name}</span> ${response.data[i].profile.last_name} M.D.</h4> 
+          <h5>${response.data[i].practices[0].visit_address.city}, ${response.data[i].practices[0].visit_address.state}</h5> 
+          <ul>
+            <li class='address'>${response.data[i].practices[0].visit_address.street}</li>
+            <li class='address address2' id='secondStreet'>${response.data[i].practices[0].visit_address.street2}</li>
+            <li class='address'>${response.data[i].practices[0].visit_address.zip}</li>
+          </ul>
+          <p class='small-italics'>It is ${response.data[i].practices[0].accepts_new_patients} that we are accepting new patients!</p>
+          <p class='website' id='missingWebsite'></p>
+          <p>☎️: ${response.data[i].practices[0].phones[0].number}</p>
+          <hr>
+        </div>`);
+        let newWebsiteID = 'website' + i;
+        $('#missingWebsite').attr('id', newWebsiteID);
+        let websiteFinder = function () {
+          if (response.data[i].practices[0].website === undefined) {
+            let el = $('#' + newWebsiteID);
+            el.html('');
+          } else {
+            let el = $('#' + newWebsiteID);
+            el.html(response.data[i].practices[0].website);
+          }
+        };
+        websiteFinder();
+        $('#secondStreet').attr('id', i); // replaces the ID of each dynamic address2 value with a different ID to use later
+        if (response.data[i].practices[0].visit_address.street2 === undefined) {
+          let el = $('#' + i);
+          el.html('');
+        }
+
+        let newMiddleNameID = 'middleName' + i;
+        $('#missingMiddleName').attr('id', newMiddleNameID); // replaces the ID of each span object with new, concatenated ID
+        if (response.data[i].profile.middle_name === undefined) {
+          let el = $('#' + newMiddleNameID);
+          el.html('');
+        }
+      }
     }
   });
 
@@ -35,12 +82,11 @@ $(document).ready(function () {
     })();
 
     function getElements(response) {
-      console.log(response);
+      $('#doctorInformation').show();
       $('#doctorInformation').text('');
       let doctorsArr = Object.keys(response.data);
-      console.log(doctorsArr);
+      noSearchResult(doctorsArr);
       for (let i = 0; i < doctorsArr.length; i++) {
-        // console.log('doctorSearchByName response: ', response.data[i].profile.first_name);
         $('#doctorInformation').append(`
         <div id='doctorResult'>
           <h4> ${response.data[i].profile.first_name} <span class='middleName' id='missingMiddleName'>${response.data[i].profile.middle_name}</span> ${response.data[i].profile.last_name} M.D.</h4> 
@@ -55,7 +101,6 @@ $(document).ready(function () {
           <p>☎️: ${response.data[i].practices[0].phones[0].number}</p>
           <hr>
         </div>`);
-        // console.log('website: ', response.data[i].practices[0].website);
         let newWebsiteID = 'website' + i;
         $('#missingWebsite').attr('id', newWebsiteID);
         let websiteFinder = function () {
@@ -63,7 +108,6 @@ $(document).ready(function () {
             let el = $('#' + newWebsiteID);
             el.html('');
           } else {
-            console.log(response.data[i].practices[0].website);
             let el = $('#' + newWebsiteID);
             el.html(response.data[i].practices[0].website);
           }
